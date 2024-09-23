82079c455be2e7ba2100924887084759c2e34c30
1997MarsRover <antonygithinji11156@gmail.com>
2024-09-23 18:44:20 +0300

    add: cuda files-ninja

--- profiler/cuda_crop_naive/main.cpp
+++ profiler/cuda_crop_naive/main.cpp
@@ -1,5 +1,5 @@
 #include <torch/extension.h>
-torch::Tensor crop_image(torch::Tensor image, torch::Tensor crop_centers, int crop_size)
+torch::Tensor crop_image(torch::Tensor image, torch::Tensor crop_centers, int crop_size);
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
 m.def("crop_image", torch::wrap_pybind_function(crop_image), "crop_image");
 }
